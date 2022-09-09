ce399bae2d24f75613c784ef77b5bf0d33623a99
benpretorius <54069207+benpretorius@users.noreply.github.com>
2022-09-09 18:33:33 +0200

    Created basic web server with 2 pages

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page.")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2+2. %d", sum))
+}
+
+// addValues adds to integers and returns the sum
+func addValues(x, y int) int {
+
+	return x + y
+}
+
+// main entry point for our application
 func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	fmt.Printf(fmt.Sprintf("Starting application on port %s", portNumber))
+	http.ListenAndServe(portNumber, nil) // The first 1024 ports is privileged. In other words you need to be a super user.
 
 }
