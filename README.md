8367368b886a39d0a1e1583246a6636ebd1f9750
benpretorius <54069207+benpretorius@users.noreply.github.com>
2022-09-12 11:59:02 +0200

    created seperate file for handlers.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Read these important things about the project ;)
+
+Run multiple Go files at the same time on a Macintosh like this:
+
+go run *.go
+
+On Windows, though, this will not work unless you have customized your IDE or terminal. Instead, use this command:
+
+go run .
+
+Note also that you can use the same command, go run ., on Mac and Linux as well.
--- main.go
+++ main.go
@@ -2,21 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.html")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.html")
-}
 
 // main is the entry point for our application
 func main() {
@@ -26,13 +15,4 @@ func main() {
 	fmt.Printf(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil) // The first 1024 ports is privileged. In other words you need to be a super user.
 
-}
-
-func renderTemplate(w http.ResponseWriter, tmplName string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmplName)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		return
-	}
 }
