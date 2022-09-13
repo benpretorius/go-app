9caf95507c76fae19cea181120af22e537162285
benpretorius <54069207+benpretorius@users.noreply.github.com>
2022-09-13 17:16:37 +0200

    added base template.

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,10 +8,14 @@ import (
 
 // Rendertemplate creates a new template from the passed files
 func RenderTemplate(w http.ResponseWriter, tmplName string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmplName)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmplName, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template: ", err)
 		return
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter, tmplName string) {
+
 }
